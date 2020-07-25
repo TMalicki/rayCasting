4c1e33f52fb602a4123a9e4c12c6cef5c2584787
Tomasz Malicki <t.malicki1@wp.pl>
2020-07-25 15:42:24 +0200

    not stable version of raycasting

--- raycast.js
+++ raycast.js
@@ -191,11 +191,179 @@ class Editor
     {
         this.realTimeView();
         this.draw();
+        this.editMode();
+        this.fadeOut(0.005);
+    }
+}
+
+class RayVision
+{
+    constructor()
+    {
+        this.playerPos = [windowWidth/2, windowHeight];
+        this.playerRadius = 5;
+        this.playerColor = 'red';
+        this.angles = [];
+        this.obstacleLines = [];
+        this.polygons = [];
+    }
+
+    getMousePosition()
+    {
+        let that = this;
+
+        canvas.mousemove(function(e) { 
+            that.playerPos = [e.clientX, e.clientY]; 
+        });
+    }
+
+    drawPlayer()
+    {
+        if(editor.editorMode == false)
+        {
+            this.getMousePosition();
+            raycast.fillStyle = this.playerColor;
+            raycast.beginPath(this)
+            raycast.arc(this.playerPos[0], this.playerPos[1], this.playerRadius, 0, 2 * Math.PI);
+            raycast.fill();   
+        }
+    }
+
+    polygonsDraw()
+    {
+        raycast.fillStyle = 'red';
+        raycast.beginPath();
+        raycast.moveTo(this.polygons[0].x, this.polygons[0].y);
+        for(let i = 1; i < this.polygons.length; i++)
+        {
+            let intersects = this.polygons[i];
+            raycast.lineTo(intersects.x, intersects.y);
+        }
+        raycast.fill();
+    }
+
+    rayCastingAlgorithm()
+    {
+        this.raysAngle();
+        this.searchingObstacleLines();
+        this.polygons = this.castRays();
+        this.polygonsDraw();
+    }
+
+    raysAngle()
+    {
+        let obstaclesAll = editor.obstacles;
+        for(let obstacleNr = 0; obstacleNr < obstaclesAll.length; obstacleNr++)
+        {
+            let pointsAll = obstaclesAll[obstacleNr].points;
+            for(let pointNr = 0; pointNr < pointsAll.length; pointNr++)
+            {
+                let actualPoint = pointsAll[pointNr];
+                let actualAngle = Math.atan2(actualPoint[1] - this.playerPos[1], actualPoint[0] - this.playerPos[0]);
+                actualPoint.angles = actualAngle;
+                this.angles.push(actualAngle - 0.00001, actualAngle, actualAngle + 0.00001);
+            }
+        }
+    }
+
+    searchingObstacleLines()
+    {
+        let obstaclesAll = editor.obstacles;
+        for(let obstacleNr = 0; obstacleNr < obstaclesAll.length; obstacleNr++)
+        {
+            let pointsAll = obstaclesAll[obstacleNr].points;
+            for(let pointNr = 1; pointNr < pointsAll.length; pointNr++)
+            {
+                this.obstacleLines.push([[pointsAll[pointNr - 1][0], pointsAll[pointNr - 1][1]], [pointsAll[pointNr][0], pointsAll[pointNr][1]]]);
+            }
+        }
+    }
+
+    castRays()
+    {
+        let intersections = [];
+        for(let angleIndex = 0; angleIndex < this.angles.length; angleIndex++)
+        {
+            let dx = Math.cos(this.angles[angleIndex]);
+            let dy = Math.sin(this.angles[angleIndex]);
+
+            // line that goes through mousePos and mousePos + dx/dy (in direction of calculated angle)
+            let ray = [[this.playerPos[0], this.playerPos[1]], [this.playerPos[0] + dx, this.playerPos[1] + dy]];   // thats casted ray
+            
+            // find closest intersection
+            let closestIntersection = null;
+            for(let index = 0; index < this.obstacleLines.length; index++)
+            {
+                let intersection = this.lineIntersection(ray, this.obstacleLines[index]);
+                // if null was returned than do nothing
+                if(!intersection) continue;
+                // if there was no closestIntersection or if intersection parameter is smaller than parameter stored in closestIntersection
+                if(!closestIntersection ||  intersection.param < closestIntersection.param)
+                {
+                    //debugger;
+                    closestIntersection = intersection;
+                }
+            }
+
+            if(!closestIntersection) continue;
+            else
+            {
+                // angle for closestIntersection
+                closestIntersection.angles = this.angles[angleIndex];
+                // add to list of intersections
+                intersections.push(closestIntersection);
+            }
+        }
+
+        //sort by angles
+        intersections = intersections.sort(function(a, b){return a.angle - b.angle;});
+
+        return intersections;
+    }
+
+    lineIntersection(ray, obstacle)
+    {
+        let x0_ray = ray[0][0];
+        let y0_ray = ray[0][1];
+        let vx_ray = ray[1][0] - x0_ray; // vx - velocity in x-axis in one frame (so basicly it is delta-x: [x-x0]);
+        let vy_ray = ray[1][1] - y0_ray;
+
+        let x0_obstacle = obstacle[0][0];
+        let y0_obstacle = obstacle[0][1];
+        let vx_obstacle = obstacle[1][0] - x0_obstacle;
+        let vy_obstacle = obstacle[1][1] - y0_obstacle;
+
+        // check for parallelism
+        let rayMagnitude = Math.sqrt(Math.pow(vx_ray,2) + Math.pow(vy_ray,2));
+        let obstacleMagnitude = Math.sqrt(Math.pow(vx_obstacle,2) + Math.pow(vy_obstacle,2));
+        if(vx_ray / rayMagnitude == vx_obstacle / obstacleMagnitude && vy_ray / rayMagnitude == vy_obstacle / obstacleMagnitude)
+        {
+            return null; // lines are parelell
+        }
+
+        // for intersection y value of obstacle is the same as y value of ray, 
+        // equations are:
+        // x0_ray + vx_ray*T1 = x0_obstacle + vx_obstacle*T2
+        // y0_ray + vy_ray*T1 = y0_obstacle + vy_obstacle*T2
+        // after some shifting:
+        let T2 = (vx_ray*(y0_obstacle - y0_ray) + vy_ray*(x0_ray - x0_obstacle)) / (vx_obstacle*vy_ray - vy_obstacle*vx_ray);
+        let T1 =  (x0_obstacle + vx_obstacle *  T2 - x0_ray) / vx_ray;
+
+        // if any of above the not intersect
+        if(T1 < 0) return null;
+        if(T2 < 0 || T2 > 1) return null;
+
+        // return object with intersectionPoint and parametr T1
+        return {x: x0_ray+vx_ray*T1,
+                y: y0_ray+vy_ray*T1,
+                param: T1
+                };
     }
 }
 
 clearWindow();
 let editor = new Editor();
+let player = new RayVision();
 
 editor.addPoint();  // that should works only in editor mode
 editor.makeFigure();    // that should works only in editor mode
@@ -205,4 +373,4 @@ editor.makeFigure();    // that should works only in editor mode
 //$("document").on("keypress",  checkKey);
 
 document.addEventListener("keypress", checkKey);
-setInterval(function() { clearWindow(); editor.editor(); editor.editMode(); editor.fadeOut(0.005); }, 1000/60);
+setInterval(function() { clearWindow(); editor.editor(); player.drawPlayer(); if(editor.editorMode == false) { player.rayCastingAlgorithm(); }; }, 1000/60);
